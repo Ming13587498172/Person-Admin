b9f0d41c07118669701103641af209436b923ef6
MingChen <1137974530@qq.com>
2022-07-13 11:57:36 +0800

    项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: 'Person管理后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置 appMain右侧主体上面的 header是不是固定定位的
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 设置侧边栏上面的 logo是否显示
+   */
+  sidebarLogo: true
+}
